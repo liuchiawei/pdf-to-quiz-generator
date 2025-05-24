23ff7576b66adfd56295f3188d508ed088bf0b21
LIU CHIAWEI <doublecheap@gmail.com>
2025-05-25 00:24:04 +0900

    style/basic-ui: upgrade to tailwind v4

--- components/quiz.tsx
+++ components/quiz.tsx
@@ -39,7 +39,7 @@ const QuestionCard: React.FC<{
           <Button
             key={index}
             variant={
-              selectedAnswer === answerLabels[index] ? "secondary" : "outline"
+              selectedAnswer === answerLabels[index] ? "secondary" : "outline-solid"
             }
             className={`h-auto py-6 px-4 justify-start text-left whitespace-normal ${
               showCorrectAnswer && answerLabels[index] === question.answer
@@ -55,7 +55,7 @@ const QuestionCard: React.FC<{
             <span className="text-lg font-medium mr-4 shrink-0">
               {answerLabels[index]}
             </span>
-            <span className="flex-grow">{option}</span>
+            <span className="grow">{option}</span>
             {(showCorrectAnswer && answerLabels[index] === question.answer) ||
               (selectedAnswer === answerLabels[index] && (
                 <Check className="ml-2 shrink-0 text-white" size={20} />
